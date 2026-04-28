3686ceff3f4547a6b0f1959dc3d426d522681742
KohJinSong <s10275044@connect.np.edu.sg>
2026-04-28 11:23:14 +0800

    task 2 & 3 for Week 2
    adding rotation for the X.
    making the object move on the X

--- Assets/NewComponent.cs
+++ Assets/NewComponent.cs
@@ -8,18 +8,37 @@ public class NewComponent : MonoBehaviour
         
     }
 
-    Vector3 valueToMove = new Vector3(0.005f,0,0.01f);
+    Vector3 valueToMove = new Vector3(0.05f,0,0);
+    bool movingRight = true;
 
     // Update is called once per frame
     void Update()
     {
         NewMethod();
     }
-
+    public float rotateSpeed = 180f;
     private void NewMethod()
     {
-        transform.localPosition += valueToMove;
-        transform.localScale += valueToMove;
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0f, 0f);
+        float Movement = transform.localPosition.x;
+        if (movingRight)
+        {
+            transform.localPosition += valueToMove;
+            // transform.localRotation = Quaternion.Euler(0, 0, 0);
+            if (Movement >= 10f)
+            {
+                movingRight = false;
+            }
+        }
+        else
+        {
+            transform.localPosition -= valueToMove;
+            // transform.localRotation = Quaternion.Euler(0, 180f, 0);
+            if (Movement <= -10f)
+            {
+                movingRight = true;
+            }
+        }
 
         print(transform.localPosition.y);
         print(transform.localPosition.x);
